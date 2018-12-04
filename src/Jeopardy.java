8e655d8b1f4462ca4b9afdd0f16cb01e982112e4
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-03 17:46:30 -0800

    Jeopardy.java, Movie.java, Netflix.java

--- src/Jeopardy.java
+++ src/Jeopardy.java
@@ -8,7 +8,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
 
-
 import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -18,8 +17,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
-
 
 /* Check out the Jeopardy Handout to see what the end result should look like: http://bit.ly/1bvnvd4 */
 
@@ -44,39 +41,40 @@ public class Jeopardy implements ActionListener {
 		frame.setLayout(new BorderLayout());
 
 		// 1. Make the frame show up
-frame.setVisible(true);
+		frame.setVisible(true);
 		// 2. Give your frame a title
-frame.setTitle("Jeopardy");
+		frame.setTitle("Jeopardy");
 		// 3. Create a JPanel variable to hold the header using the createHeader method
-JPanel panel=createHeader("Jeopardy");
+		JPanel panel = createHeader("Jeopardy");
 		// 4. Add the header component to the quizPanel
-quizPanel.add(panel);
+		quizPanel.add(panel);
 		// 5. Add the quizPanel to the frame
-frame.add(quizPanel);
+		frame.add(quizPanel);
 		// 6. Use the createButton method to set the value of firstButton
-firstButton=createButton("$100");
+		firstButton = createButton("$100");
 		// 7. Add the firstButton to the quizPanel
-quizPanel.add(firstButton);
-		// 8. Write the code to complete the createButton() method below. Check that your
+		quizPanel.add(firstButton);
+		// 8. Write the code to complete the createButton() method below. Check that
+		// your
 		// game looks like Figure 1 in the Jeopardy Handout - http://bit.ly/1bvnvd4.
 
 		// 9. Use the secondButton variable to hold a button using the createButton
 		// method
-secondButton=createButton("$200");
+		secondButton = createButton("$200");
 		// 10. Add the secondButton to the quizPanel
-quizPanel.add(secondButton);
+		quizPanel.add(secondButton);
 		// 11. Add action listeners to the buttons (2 lines of code)
-firstButton.addActionListener(this);
-secondButton.addActionListener(this);
+		firstButton.addActionListener(this);
+		secondButton.addActionListener(this);
 		// 12. Write the code to complete the actionPerformed() method below
 
 		// 13. Add buttons so that you have $200, $400, $600, $800 and $1000 questions
 		quizPanel.add(firstButton, secondButton);
-		 /*
+		/*
 		 * [optional] Use the showImage or playSound methods when the user answers a
 		 * question
-		 */		
-		
+		 */
+
 		frame.pack();
 		quizPanel.setLayout(new GridLayout(buttonCount + 1, 3));
 		frame.add(makeScorePanel(), BorderLayout.NORTH);
@@ -84,70 +82,75 @@ secondButton.addActionListener(this);
 				Toolkit.getDefaultToolkit().getScreenSize().width);
 	}
 
-
 	private JButton createButton(String dollarAmount) {
-		
+
 		// Create a new JButton
-JButton button=new JButton();
+		JButton button = new JButton();
 		// Set the text of the button to the dollarAmount
-button.setText(dollarAmount);
+		button.setText(dollarAmount);
 		// Increment the buttonCount (this should make the layout vertical)
-buttonCount++;
+		buttonCount++;
 		// Return your new button instead of the temporary button
 
 		return button;
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		
+
 		// Remove this temporary message after testing:
-		
 
 		JButton buttonPressed = (JButton) e.getSource();
 		// If the buttonPressed was the firstButton
-if(buttonPressed==firstButton) {
-	askQuestion("What is 2+2?", "4", 300);
-}
-			// Call the askQuestion() method
- if(buttonPressed==secondButton) {
-	 askQuestion("What is pi?", "3.14", 500 );
- }
-		// Complete the code in the askQuestion() method. When you play the game, the score should change.
+		if (buttonPressed == firstButton) {
+			askQuestion("What is 2+2?", "4", 100);
+		}
+		// Call the askQuestion() method
+		if (buttonPressed == secondButton) {
+			askQuestion("What is pi?", "3.14", 200);
+		}
+		// Complete the code in the askQuestion() method. When you play the game, the
+		// score should change.
 
 		// If the buttonPressed was the secondButton
 
-			// Call the askQuestion() method with a harder question
+		// Call the askQuestion() method with a harder question
 
-		// Clear the text on the button that was pressed (set the button text to nothing)
+		// Clear the text on the button that was pressed (set the button text to
+		// nothing)
 	}
 
 	private void askQuestion(String question, String correctAnswer, int prizeMoney) {
-		
-		// Use the playJeopardyTheme() method to play music while the use thinks of an answer
+
+		// Use the playJeopardyTheme() method to play music while the use thinks of an
+		// answer
 		playJeopardyTheme();
-		// Remove this temporary message and replace it with a pop-up that asks the user the question
-		String answer=JOptionPane.showInputDialog(question);
-	
-		// Stop the theme music when they have entered their response. Hint: use the sound variable 
+		// Remove this temporary message and replace it with a pop-up that asks the user
+		// the question
+		String answer = JOptionPane.showInputDialog(question);
+
+		// Stop the theme music when they have entered their response. Hint: use the
+		// sound variable
 		sound.stop();
 		// If the answer is correct
-if(answer.equals(correctAnswer)) {
-	prizeMoney++;
-	JOptionPane.showMessageDialog(null, "Correct!");
-}else {
-	prizeMoney--;
-	JOptionPane.showMessageDialog(null, "Wrong! It's "+ correctAnswer);
-	updateScore();
-}
-			// Increase the score by the prizeMoney
+		if (answer.equals(correctAnswer)) {
+			score+=prizeMoney;
+			JOptionPane.showMessageDialog(null, "Correct!");
+		} else {
+			score-=prizeMoney;
+			JOptionPane.showMessageDialog(null, "Wrong! It's " + correctAnswer);
+					}
+		updateScore();
 
-			// Pop up a message to tell the user they were correct
+		// Increase the score by the prizeMoney
+
+		// Pop up a message to tell the user they were correct
 
 		// Otherwise
 
-			// Decrement the score by the prizeMoney
+		// Decrement the score by the prizeMoney
 
-			// Pop up a message to tell the user they were wrong and give them the correct answer
+		// Pop up a message to tell the user they were wrong and give them the correct
+		// answer
 
 		// Call the updateScore() method
 
---
+++ src/Netflix.java
@@ -0,0 +1,11 @@
+
+
+public class Netflix {
+public static void main(String[] args) {
+	 Movie m=new Movie("Avengers: Infinity War",10);
+	m.getTitle();
+	m.getRating();
+	m.getTicketPrice("$15");
+	m.compareTo(otherMovie);
+	}
+}
