a630abd517202f8b64759dd0f935d45d082ae396
ilabstaff <ilabstaff@172.16.116.79>
2019-01-14 17:16:52 -0800

    checkPoint.java, Jam.java

---
+++ src/Jam.java
@@ -0,0 +1,13 @@
+
+public class Jam {
+	
+String flavor;
+Jam(String flavor){
+	this.flavor=flavor;
+}
+
+void spreadOnToast() {
+	System.out.println("Spreading "+flavor+" jam on toast" );
+	}
+
+}
---
+++ src/checkPoint.java
@@ -0,0 +1,32 @@
+
+public class checkPoint {
+	public static void main(String[] args) {
+		
+	
+//1.) Cat("bob", 4);
+	
+//2.) JFrame, JLabel, JButton
+	
+////3.) 
+//		public name getname(String name);
+//	if(name.equals("your name")) {
+//		System.out.println("awesome");
+//	}else {
+//		System.out.println("lame");
+//	}
+	
+//4.) 
+	//int numShoes=2;
+	//String iceCream="vanilla";
+	//String dance="NO";
+	//JButton button= new JButton();
+	//String Cat="Felix";
+	
+	
+//5.) In Jam.java
+	
+Jam j=new Jam("StrawBerry");
+j.spreadOnToast();
+	
+	}
+}
